e2ed462b06313129da652be188c5b3362f90d5c0
josielynngriffin <jelynngriffin@gmail.com>
2023-08-07 22:03:16 -0400

    Add basic boilerplate for components

---
+++ client/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <h4>feev.io</h4>
+            <a href="https://github.com/Cat-Scratch-Feev/trivia-feev" target="_blank"><i className="footer__icon fa-brands fa-github"></i></a>
+            <hr></hr>
+            <small>©2023 cat-scratch feev | © feev.io</small>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ client/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <div>
+           <i class="fa-solid fa-cat fa-2xl"></i>
+           <h1>feev.io</h1> 
+        </div>
+    )
+};
+export default Header;
