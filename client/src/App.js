7875b6840c71829259e13dfd8410753f50ee0d84
josielynngriffin <jelynngriffin@gmail.com>
2023-08-07 22:33:14 -0400

    Add imports, router, Apollo
    Routes added by Mauvonte Roberts

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+// Component Imports
+import Header from './components/header.js';
+import SideNav from './components/side-nav.js';
+// Page Imports
+import Home from './pages/home.js';
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+ 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Router>
+        <div className="container">
+            <SideNav/>
+            {/* Wrap Route elements in a Routes component */}
+            <Routes>
+              {/* Define routes using the Route component to render different page components at different paths */}
+            
+              {/* Define a default route that will render the Home component */}
+              <Route 
+                path="/" 
+                element={<Home />} 
+              />
+              {/* Define a route that will take in variable data */}
+              {/* <Route 
+                path="/profiles/:profileId" 
+                element={<Profile />} 
+              /> */}
+            </Routes>
+          </div>
+      </Router>
     </div>
+    </ApolloProvider>
   );
 }
 
