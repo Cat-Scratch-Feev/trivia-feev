565a6bf66cf9a27574c02ba181a2b57581132fe7
josielynngriffin <jelynngriffin@gmail.com>
2023-08-09 11:50:05 -0400

    Add header component and styling

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -3,9 +3,20 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-           <i class="fa-solid fa-cat fa-2xl"></i>
-           <h1>feev.io</h1> 
+        <div className="header__wrap">
+            <div className="header__name">
+                <i className="fa-solid fa-cat header__name--icon"></i>
+                <h1>feev<span className="header__name--color">.io</span></h1> 
+            </div>
+           <div className="dropdown">
+                <button className="btn  dropdown-toggle user__nav--button" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <i className="fa-solid fa-user"></i>
+                </button>
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#"><i class="fa-solid fa-gear"></i> settings</a></li>
+                    <li><a className="dropdown-item" href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i> sign out</a></li>
+                </ul>
+            </div>
         </div>
     )
 };
