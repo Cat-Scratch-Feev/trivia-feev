2c8ddda329dcdeb0f39a1896c423a4e56e7b55bb
josielynngriffin <jelynngriffin@gmail.com>
2023-08-09 12:52:49 -0400

    Add side nav, styling, and page styling

--- client/src/components/side-nav.js
+++ client/src/components/side-nav.js
@@ -3,8 +3,27 @@ import React from 'react';
 
 const SideNav = () => {
     return (
-        <div>
+        <div className="sideNav__wrap">
+            <nav>
+                <ul className="sideNav__list">
+                    <li><i class="fa-solid fa-house"></i> home</li>
+                    <li>
+                        <p>
+                                <i class="fa-solid fa-list"></i> trivia <i class="fa-solid fa-caret-down"type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample"></i>
+                        </p>
+                        <div>
+                        <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                            <div class="card card-body">
+                            This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
+                            {/* Eventually place links here */}
+                            </div>
+                        </div>
+                        </div></li>
+                    
+                    <li><i class="fa-solid fa-trophy"></i> leaders</li>
+                </ul>
             
+            </nav>
         </div>
     )
 };
