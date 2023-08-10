031f3ac1b40613defe9aa8ad5ecf8dad7f28d07d
josielynngriffin <jelynngriffin@gmail.com>
2023-08-10 15:38:34 -0400

    Add error handling, fix inconsistent values

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -9,17 +9,22 @@ class AuthService {
   // Checks if there is a valid token or not
   loggedIn() {
     const token = this.getToken();
-    return token && !this.expiredToken(token) ? true : false;
+    return !!token && !this.expiredToken(token) ? true : false;
   }
 
   // Check token expiration
-  expiredToken() {
-    const decodedToken = decode(token);
-    if (decodedToken.exp < Date.now() / 1000) {
-      localStorage.removeItem("id_token");
-      return true;
+  expiredToken(token) {
+    try{
+      const decodedToken = decode(token);
+      if (decodedToken.exp < Date.now() / 1000) {
+        //localStorage.removeItem("auth_token");
+        this.logout();
+        return true;
+      } else return false;
+    } catch(err) {
+      return false;
     }
-    return false;
+    
   }
 
   // Gets token from local storage
@@ -29,7 +34,7 @@ class AuthService {
 
   // Sets token to local storage and reloads page
   login(authToken) {
-    localStorage.setItem("auth_token", Token);
+    localStorage.setItem("auth_token", authToken);
     window.location.assign("/");
   }
 
