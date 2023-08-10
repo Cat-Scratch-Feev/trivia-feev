da77b87b9deb938b01f7de544f097aeab49fa94b
josielynngriffin <jelynngriffin@gmail.com>
2023-08-10 15:43:58 -0400

    Add basic react page structure to landing page

--- client/src/pages/landing-page.js
+++ client/src/pages/landing-page.js
@@ -1,1 +1,10 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div className="feev__landing-wrap">
+            <p>landing page!</p>
+        </div>
+    );
+};
 export default Landing;
