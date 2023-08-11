86ac8232accd70dc49b2193883280fff229b2102
James Farris <jafarris.exe@gmail.com>
2023-08-10 20:19:22 -0400

    User and score by ID's added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,13 +15,35 @@ export const QUERY_USERS = gql`
 
 // Get a single user by username
 export const QUERY_ONE_USER = gql`
-    query user($username: String!) {
-        user(username: $username) {
-            _id
-            username
-            email
-            password
-            score
-        }
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      password
+      score
     }
+  }
+`;
+
+// Get a single user by id
+export const QUERY_USER_BY_ID = gql`
+  query user($id: ID!) {
+    user(_id: $id) {
+      _id
+      username
+      email
+      password
+      score
+    }
+  }
+`;
+
+// Get a score by user id
+export const QUERY_SCORE = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      score
+    }
+  }
 `;
