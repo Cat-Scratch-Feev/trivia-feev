e98bc2ec86c87acc6b89be3225b2945768b3c4f5
James Farris <jafarris.exe@gmail.com>
2023-08-10 20:11:24 -0400

    Added single user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,3 +14,14 @@ export const QUERY_USERS = gql`
 `;
 
 // Get a single user by username
+export const QUERY_ONE_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            password
+            score
+        }
+    }
+`;
