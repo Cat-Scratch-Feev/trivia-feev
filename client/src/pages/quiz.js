1c9c97da28a80a12521f20cf7259a88deaf7fe1b
josielynngriffin <jelynngriffin@gmail.com>
2023-08-13 21:02:15 -0400

    Refactor code to decode and shuffle in UseEffect, avoiding unwanted shuffling
    Add time penalty if wrong answer is given

--- client/src/pages/quiz.js
+++ client/src/pages/quiz.js
@@ -12,6 +12,9 @@ const Quiz = ({quizState, setQuizState}) => {
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [score, setScore] = useState(0);
 
+    const [timer, setTimer] = useState(100);
+    const [decodedCurrentQuestion, setDecodedCurrentQuestion] = useState(null);
+    const [shuffledAnswers, setShuffledAnswers] = useState([]);
     // Our api url
     let url = `https://opentdb.com/api.php?amount=10&category=${selectCat}&type=multiple`
 
@@ -23,10 +26,12 @@ const Quiz = ({quizState, setQuizState}) => {
                 setQuestions(data.results);
             });
     };
+
     // Fetch as page loads
     useEffect(() => {
         fetchQuizData();
-    }, [])
+    }, []);
+
     // Function to shuffle options
     const shuffleArray = (array) => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -35,10 +40,15 @@ const Quiz = ({quizState, setQuizState}) => {
         }
         return array;
     };
+
     // Handle Click
     const handleOptionClick = selectedOption => {
+        //Add points to score if answer is correct
         if (selectedOption === currentQuestionData?.correct_answer) {
             setScore(score + 10);
+        }  else {
+            // Deduct five seconds from the timer if the answer is incorrect
+            setTimer(prevTimer => (prevTimer >= 5 ? prevTimer - 5 : 0));
         }
         if (currentQuestion === finalQuestion - 1){
             setQuizState('end');
@@ -49,42 +59,79 @@ const Quiz = ({quizState, setQuizState}) => {
         // Move to the next question
         
     };
+
     // Data for current question
     const currentQuestionData = questions[currentQuestion];
     const finalQuestion = questions.length;
-
-    //Decode html using 'he' library to avoid encoded characters in default api response
-    const decodedCurrentQuestion = currentQuestionData
-    ? {
-        ...currentQuestionData,
-        question: he.decode(currentQuestionData.question),
-        correct_answer: he.decode(currentQuestionData.correct_answer),
-        incorrect_answers: currentQuestionData.incorrect_answers.map(he.decode),
-      }
-    : null;
-    console.log(decodedCurrentQuestion);
-    console.log('Decoded Question:', decodedCurrentQuestion?.question);
-
-    //Shuffle current decoded data
-    const shuffledAnswers = decodedCurrentQuestion
-    ? shuffleArray([...decodedCurrentQuestion.incorrect_answers, decodedCurrentQuestion.correct_answer])
-    : [];
-
+    
+    useEffect(() => {
+        let timerInterval;
+        
+        if (quizState === 'quiz') {
+            timerInterval = setInterval(() => {
+                setTimer(prevTimer => {
+                    if (prevTimer === 0) {
+                        setQuizState('end'); // End the quiz when timer reaches 0
+                    }
+                    return prevTimer > 0 ? prevTimer - 1 : prevTimer;
+                });
+            }, 1000);
+        }
+        
+        return () => {
+            clearInterval(timerInterval);
+        };
+    }, [quizState]);
+    
     const handleStartQuiz = () => {
         //Transition to the quiz state when the start button is clicked
         setQuizState('quiz');
     }
 
+    //Decode and shuffle answers when a new question is rendered
+    useEffect(() => {
+        //Decode html using 'he' library to avoid encoded characters in default api response
+        if (currentQuestionData) {
+          const decodedQuestion = {
+            ...currentQuestionData,
+            question: he.decode(currentQuestionData.question),
+            correct_answer: he.decode(currentQuestionData.correct_answer),
+            incorrect_answers: currentQuestionData.incorrect_answers.map(he.decode),
+          };
+          setDecodedCurrentQuestion(decodedQuestion);
+
+        //Shuffle answers to prevent fixed order when rendering
+          const newShuffledAnswers = shuffleArray([
+            ...decodedQuestion.incorrect_answers,
+            decodedQuestion.correct_answer,
+          ]);
+          setShuffledAnswers(newShuffledAnswers);
+        }
+      }, [currentQuestionData]);
+
     return (
         <div className="feev__home">
-            {quizState === 'start' && (
-                <div>test
+            {quizState === 'start' && questions.length > 0 && currentQuestionData ? (
+                <div className="quiz__wrap">
+                    <section className="directions__header">
+                        <h2>{currentQuestionData.category}</h2>
+                        <div className="timer__block">
+                            <i class="fa-solid fa-stopwatch"></i>
+                            <p>{timer} seconds</p>
+                        </div>
+                        
+                    </section>
+                    <h3>you have chosen the {currentQuestionData.category} category!</h3>
+                    <p>you will have 10 seconds per question, with a 5 second penalty for incorrect answers.</p>
+                    <p>your quiz will begin when you press start!</p>
                     <button onClick={handleStartQuiz}>start</button>
                 </div>
                 
+            ) : (
+                <></>
             )}
             {quizState === 'quiz' && questions.length > 0 && currentQuestionData ?  (
-                <>
+                <div className="quiz__wrap">
                     <h2>{currentQuestionData.category}</h2>
                     <p>{decodedCurrentQuestion.question}</p>
                     <div className="option-holder">
@@ -98,16 +145,20 @@ const Quiz = ({quizState, setQuizState}) => {
                             </button>
                         ))}
                     </div>
-                    <p>This is the timer space</p>
+                    <p>Time remaining: {timer} s</p>
                     <p>Score: {score}</p>
-                </>
+                </div>
             ) : (
                 <></>
             )}
-            {quizState === 'end' &&(
-                <div>
-                    <p>final score: {score}</p>
+            {quizState === 'end' && questions.length > 0 && currentQuestionData ?  (
+                <div className="quiz__wrap">
+                    <h2 className="quiz__category">{currentQuestionData.category}</h2> 
+                    <p>congrats! you scored: <i class="fa-solid fa-coins"></i> {score}</p>
+                    <p>your new total score: <i class="fa-solid fa-coins"></i> {score}</p>
                 </div>
+            ) : (
+                <></>
             )}
         </div>
     );
