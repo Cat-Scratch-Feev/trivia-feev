c9755206abc9208ba4036b22024fd0ce7d4f7616
Mauvonte Roberts <moeroberts183@gmail.com>
2023-08-14 22:01:04 -0400

    working on the home.js css

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 
-const Home = () => {
+
+function Home() {
     return (
         <div className="feev__home">
-            
+
+            <div className='greet-bg'>
+
+                <p className='user-greeting'>Welcome, User! </p>
+                <p className='score'>Score: 1200 Points
+                 
+
+                    <i className=" fa-solid fa-coins">
+                    
+                    </i> 
+                
+                </p>
+
+
+            </div>
+
         </div>
     );
-};
+}
 export default Home;
