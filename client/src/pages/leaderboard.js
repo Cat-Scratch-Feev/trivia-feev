5932d8cb49f4b2e3ef740bfd93c751819781e036
dhs88103 <dhs88103@gmail.com>
2023-08-14 20:22:22 -0400

    working on styling of leaderboard card

--- client/src/pages/leaderboard.js
+++ client/src/pages/leaderboard.js
@@ -8,7 +8,9 @@ const LeaderboardPage = () => {
     return (
         <div className="feev__leaderboard-page-wrap">
            <div className="feev__leaderboard-wrap">
-            
+            <div className='feev__leaderboard-card'>
+                <p className="feev__leaderboard-message">All Time Leaderboard: </p>
+            </div>
            </div>
         </div>
     )
