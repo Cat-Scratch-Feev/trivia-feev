c19f1098c1855f908b41ae3b47a1f61ceeb424e3
dhs88103 <dhs88103@gmail.com>
2023-08-16 21:25:56 -0400

    added leaderboard components and linked to pages

--- client/src/pages/leaderboard.js
+++ client/src/pages/leaderboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import LeaderBoard from '../components/leaderboard.js';
 
 // TODO work on front end styling of leaderboard page
 
@@ -8,8 +8,9 @@ const LeaderboardPage = () => {
     return (
         <div className="feev__leaderboard-page-wrap">
            <div className="feev__leaderboard-wrap">
-            <div className='feev__leaderboard-card'>
+            <div className="feev__leaderboard-card">
                 <p className="feev__leaderboard-message">All Time Leaderboard: </p>
+                <LeaderBoard/>
             </div>
            </div>
         </div>
