4c41772bd5b786b987ddcef7889d6fe938bb5d79
josielynngriffin <jelynngriffin@gmail.com>
2023-08-17 13:44:44 -0400

    Modify leaderboard for styling

--- client/src/pages/leaderboard.js
+++ client/src/pages/leaderboard.js
@@ -7,16 +7,14 @@ import LeaderBoard from '../components/leaderboard.js';
 const LeaderboardPage = () => {
     return (
         <div className="feev__leaderboard-page-wrap">
-           <div className="feev__leaderboard-wrap">
             <div className="feev__leaderboard-card">
-                <div classname="feev__leaderboard-message">
+                <div className="feev__leaderboard-message">
                     <h2>feev<span className="header__name--color">.io</span>'s leaderboard</h2>
                 </div>
-                <div classname="feev__leaderboard-display">
+                <div className="feev__leaderboard-display">
                 <LeaderBoard/>
                 </div>
             </div>
-           </div>
         </div>
     )
 }
